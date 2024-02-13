b79941c43f04d4737994ff5dda5cda8ae41e661a
beckham-4567 <thavizup.de.66@ubu.ac.th>
2024-02-13 21:18:55 +0700

    Streamlit

---
+++ app02.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('https://preview.redd.it/since-dmc-5s-release-vergil-has-become-an-iconic-character-v0-8a0wds8pzwmb1.jpg?width=1080&crop=smart&auto=webp&s=d5c0da91cdcd31e0c793168bb8f579a22241c2d0')
+st.text_input("What's your name?")
+# git config --global user.name beckham-4567
+# git config --global user.email thavizup.de.66@ubu.ac.th
